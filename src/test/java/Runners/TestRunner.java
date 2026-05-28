cf8eab0af80cf99adfa64a1bbcec985e3423f2c3
kumar <priy.santhosh1992@gmail.com>
2026-05-28 13:31:44 +0530

    addded

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "D:\\eclipse-workspace\\vinothqaacademy\\src\\test\\resources\\features\\RegFormsUsingDB.feature", 
+		features = "D:\\eclipse-workspace\\vinothqaacademy\\src\\test\\resources\\features\\regformfill.feature", 
 		glue = "stepDefinitions", 
 		plugin = {"pretty", "html:target/cucumber-report.html" }, 		 
 		monochrome = true, 
